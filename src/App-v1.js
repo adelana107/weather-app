ca47b275c2caf14339d89421367a5f8d8e24ce5d
Adelana107 <adelana787898@gmail.com>
2025-10-15 19:44:16 +0100

    Removed Boilerplate Code With Class Fields

--- src/App-v1.js
+++ src/App-v1.js
@@ -33,18 +33,20 @@ function formatDay(dateStr) {
 }
 
 class App extends React.Component {
-  state = {
-    location: "lisbon",
-    isLoading: false,
-    displayLocation: "",
-    weather: {},
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "lisbon",
+      isLoading: false,
+      displayLocation: "",
+      weather: {},
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
 
-  fetchWeather = async () => {
+  async fetchWeather() {
     try {
       this.setState({ isLoading: true });
-
-      // Get coordinates
       const geoRes = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
       );
@@ -54,44 +56,37 @@ class App extends React.Component {
 
       const { latitude, longitude, timezone, name, country_code } =
         geoData.results.at(0);
-
       this.setState({
         displayLocation: `${name} ${convertToFlag(country_code)}`,
       });
 
-      // Get weather data
       const weatherRes = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
       );
       const weatherData = await weatherRes.json();
-
       this.setState({ weather: weatherData.daily });
     } catch (err) {
       console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
-  };
+  }
 
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-
         <div>
           <input
             type="text"
-            placeholder="Search for a location..."
+            placeholder="Search from Location..."
             value={this.state.location}
             onChange={(e) => this.setState({ location: e.target.value })}
           />
         </div>
-
         <button onClick={this.fetchWeather}>Get weather</button>
-
         {this.state.isLoading && <p className="Loader">Loading...</p>}
-
-        {this.state.weather?.weathercode && (
+        {this.state.weather.weathercode && (
           <Weather
             weather={this.state.weather}
             location={this.state.displayLocation}
